9c8cbe265f69db6fe24b77b5012ff01fe382b6fd
Luis <33837673+lfresnog@users.noreply.github.com>
2020-05-29 16:29:29 +0200

    Update Search.jsx

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -7,7 +7,7 @@ function Search() {
     return (
       <div className='search'>
         <input className='searchInput' onChange={event=>{context.name.set(event.target.value);context.page.set(1)}} placeholder='Search'/>
-        <span className='status' onClick={()=>context.status.set(context.status.get==='All'?'Alive':context.status.get==='Alive'?'Dead':'All')}>
+        <span className='status' onClick={()=>{context.status.set(context.status.get==='All'?'Alive':context.status.get==='Alive'?'Dead':'All');context.page.set(1)}}>
             {context.status.get}
         </span>
       </div>
